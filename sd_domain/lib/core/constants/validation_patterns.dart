b6306fa58b16f2f4ea623d3350d1d16372807e89
Bohdan Martynov <bohdan.martynov@globallogic.com>
2020-05-28 21:40:30 +0300

    add validation_patterns

---
+++ sd_domain/lib/core/constants/validation_patterns.dart
@@ -0,0 +1,5 @@
+class ValidationPatterns {
+  static const String simpleFieldPattern = '';
+  static const String emailPattern = r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+  static const String passwordPattern = '';
+}
