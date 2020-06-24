ecf9b8560b16074968460fb4afa759b883331951
Bohdan Martynov <bohdan.martynov@globallogic.com>
2020-06-24 21:40:51 +0300

    add basic grpc communication

--- sd_data/lib/repository/auth_data_repository.dart
+++ sd_data/lib/repository/auth_data_repository.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:grpc/grpc.dart';
 import 'package:sddata/data.dart';
 import 'package:sddomain/export/domain.dart';
+import 'package:proto_module/protos.dart';
 
 class AuthDataRepository extends AuthRepository {
   final Dio _dio;
@@ -19,15 +21,29 @@ class AuthDataRepository extends AuthRepository {
   Stream<AuthTokenModel> login(
     String email,
     String password,
-  ) =>
-      _networkExecutor
-          .makeRequest(
-              _dio,
-              AuthApi.login(
-                email: email,
-                password: password,
-              ))
-          .map(_loginResponseMapper.map);
+  ) async* {
+    final channel = ClientChannel(
+      'localhost',
+      port: 50051,
+      options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
+    );
+
+    final loginClient = LoginClient(channel);
+    LoginResponse response;
+    try {
+      response = await loginClient.login(
+        LoginRequest.create()
+          ..email = email
+          ..password = password,
+      );
+    } on Object catch (e) {
+      print(e);
+    }
+
+    print('TOKEN FROM GRPC: ${response.token}');
+
+    yield AuthTokenModel(response.token);
+  }
 
   @override
   Stream<AuthTokenModel> registration(
